28e9f30432154d4a9f79d49f19790147d6d960a0
lincoln <lwdthe1@gmail.com>
2017-12-17 11:49:18 -0800

    actions menu

--- shared/js/component/ActionsMenuComponent.js
+++ shared/js/component/ActionsMenuComponent.js
@@ -3,7 +3,7 @@ SmedianPages.component.ActionsMenu = function(config) {
     if(!config.page) throw new Error('[SmedianPages.component.ActionsMenu] page is required')
 
     const self = this
-    const page = config.page
+    const _page = config.page
     var _currentUser
 
     var _openButtonSelector = '.js-SmedianPageComponentActionsMenu-openButton'
@@ -60,13 +60,53 @@ SmedianPages.component.ActionsMenu = function(config) {
             $('body').append(document.getElementById(iframeId).contentWindow.document.body.innerHTML)
             setTimeout(() => {
                 debugger
+                $('.js-SmedianPageComponentActionsMenu-showOnlyAdmin').hide()
                 _attachActions()
                 _currentUser = SmedianPages.CurrentUser.get()
+
                 if (_currentUser) {
                     $('.js-SmedianPageComponentActionsMenu-currentUserAvatarContainer').show()
                     $('.js-SmedianPageComponentActionsMenu-currentUserAvatar').attr('src', _currentUser.imageUrl)
                 } else {
                     $('.js-SmedianPageComponentActionsMenu-nonCurrentUserAvatarContainer').show()
+                }
+
+                if (!_page.currentUserIsPageAdmin) {
+                    $('.js-SmedianPageComponentActionsMenu-containerMyPagesLink').show()
+                    $('.js-SmedianPageComponentActionsMenu-myPagesLink').attr('href', `/pages/user/${_currentUser.id}`)
+                    if (_currentUser && _currentUser.homePageId) {
+                        $('.js-SmedianPageComponentActionsMenu-myPagesLink').text('All My Pages')
+                    } else {
+                        $('.js-SmedianPageComponentActionsMenu-myPagesLink').text('Make Mine')
+                    }
+                }
+
+                if(_page.currentUserIsPageAdmin) {
+                    $('.js-SmedianPageComponentActionsMenu-showOnlyAdmin').show()
+                    $('.js-SmedianPageComponentActionsMenu-adminEditPageLink').attr('href', `/page/${_page.id}/_/admin`)
+                    $('.js-SmedianPageComponentActionsMenu-adminAllPagesLink').attr('href', `/pages/${_page.masterObjectType}/${_page.masterObjectId}`)
+                } else {
+                    $('.js-SmedianPageComponentActionsMenu-showOnlyAdmin').hide()
+                }
+
+                if (!_page.currentUserIsPageAdmin && _page.masterObjectType == 'user') {
+                    $('.js-SmedianPageComponentActionsMenu-containerChatLink').show()
+                    $('.js-SmedianPageComponentActionsMenu-chatLink').attr('href', `/chat/${_page.masterObject.chatInviteUrl}`)
+                    $('.js-SmedianPageComponentActionsMenu-chatLink').text(`Chat With ${page.masterObject.name || page.masterObject.username}`)
+                } else {
+                    $('.js-SmedianPageComponentActionsMenu-containerChatLink').hide()
+                }
+
+                if (_page.masterObjectType == 'user') {
+                    var el = $('.js-SmedianPageComponentActionsMenu-masterUserProfileLink')
+                    el.attr('href', `/writer/${_page.masterObjectId}/profile`)
+                    el.text(`${page.masterObject.name || page.masterObject.username} Profile`)
+                    el.show()
+                } else {
+                    var el = $('.js-SmedianPageComponentActionsMenu-masterPubProfileLink')
+                    el.attr('href', `/p/${_page.masterObjectId}?settings=1`)
+                    el.text(`${page.masterObject.title} Dashboard`)
+                    el.show()
                 }
             }, 250)
         }
